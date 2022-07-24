35514ce63a5d6820b20a53154baed1dfba11e154
Margareta Adajuc <margaretaadajuc@gmail.com>
2022-07-24 23:43:40 +0300

    implemented the functionality to allow certain currencies

---
+++ PaySys/Server/Controllers/CurrencyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaySys.Shared;
+using System.Collections.Generic;
+
+namespace PaySys.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        [HttpGet]
+        public CurrencyList GetCurrencies()
+        {
+            return new CurrencyList
+            {
+                CurrencyNames = new List<string>
+                {
+                    "EUR",
+                    "MDL",
+                    "USD",
+                    "EC"
+                }
+            };
+        }   
+    }
+}
---
+++ PaySys/Shared/CurrencyList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaySys.Shared
+{
+    public class CurrencyList
+    {
+        public List<string> CurrencyNames { get; set; }
+    }
+}
